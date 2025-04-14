8c81641e52331d47802f263508ae18bad3404215
André Maltez Nunes <andremalteznunes@sapo.pt>
2025-04-14 13:32:51 +0100

    feat(stdout): Add functions to handle basic logging needs

---
+++ header/Utils/Logs.h
@@ -0,0 +1,9 @@
+#ifndef INCLUDE_UTILS_LOGS_H
+#define INCLUDE_UTILS_LOGS_H
+
+void info(char *msg);
+void warn(char *msg);
+void error(char *msg);
+void logAppend(char *msg);
+
+#endif /* INCLUDE_UTILS_LOGS_H */
