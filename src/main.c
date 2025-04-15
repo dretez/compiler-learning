e081e956635272634d21d598a0cf980ae304624b
André Maltez Nunes <andremalteznunes@sapo.pt>
2025-04-16 00:24:16 +0100

    feat: Implement lexer into main.c
    - Replace some of the older code, which attempted to jump the lexical
      analysis step straight into parsing.
    - Add lexical analysis results visualizer.

--- src/main.c
+++ src/main.c
@@ -1,17 +1,12 @@
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
 #include <unistd.h>
 
+#include "Lexer/Token.h"
 #include "Utils/Logs.h"
-#include "tree.h"
-
-/* Grammar:
- *    list -> list + digit
- *    list -> list - digit
- *    list -> digit
- *    digit -> 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9
- */
+#include "Utils/String.h"
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
@@ -28,47 +23,21 @@ int main(int argc, char *argv[]) {
         exit(EXIT_FAILURE);
     }
 
-    char c = '\0';
-    Tree *tree = NULL;
-    while (read(f, &c, sizeof(char))) {
-        if (c >= '0' && c <= '9') {
-            Tree *list = newTree("list", 4);
-            treeAddChild(list, newDigitNode(&c));
-            tree = list;
-        } else if (c == '+' || c == '-') {
-            Tree *oper = newTree(&c, 1);
-            Tree *list = newTree("list", 4);
-            treeAddChild(list, tree);
-            tree = list;
-            if (!read(f, &c, sizeof(char)))
-                exit(EXIT_FAILURE);
-            treeAddChild(list, newDigitNode(&c));
-            treeAddChild(list, oper);
-        }
-    }
-    printTree(tree, 0);
+    /**************************** LEXICAL ANALYSIS ****************************/
 
-    printf("\n");
+    TokenList lex = newTokenList();
+    for (Token token = nextToken(f); token.type != EOF_TOKEN;
+         token = nextToken(f)) {
+        tlistAddToken(&lex, token);
+    }
+    for (uint i = 0; i < lex.count; i++) {
+        // For temporary visualization only
+        printString(lex.list[i].str);
+        printf("\n");
+    }
+
+    printf("============================\n");
     info("Press any key to exit");
     fgetc(stdin);
     exit(EXIT_SUCCESS);
 }
-
-/* 9-5+2
- * reads '9'
- *      new tree with label 9, label len 1 and 0 children
- *      new tree with label digit, label len 5 and 1 child, the tree created
- *       before
- *      new tree with label list, label len 4 and 1 child, the tree created
- *       before
- *      sets the root to the list tree
- * reads '-'
- *      new tree with label -, label len 1 and 0 children
- *      new tree with label list, label len 4, 2 children, last list tree
- *      read next char
- *      reads 5
- *          new tree with label 5
- *          new tree with label digit
- *          add tree to list tree
- *      set root to new list tree
- */
