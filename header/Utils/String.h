d7fedd100dd1ce4a0956e279fd16f2bb094c8986
André Maltez Nunes <andremalteznunes@sapo.pt>
2025-04-15 01:57:21 +0100

    feat(String): Add field and auxiliary functions
    - Add allocSize field, allowing for a string's size to be smaller than
      the allocated memory. This is useful for allocating memory ahead of
      time whenever many resize operations are required, like when a string
      has to be copied char by char.
    - Add freeString function. Frees the and resets char pointer to NULL, to
      avoid dangling pointers, and sets the len and size fields to 0.
    - Add resizeStringAlloc. Reallocs the char pointer and, in case of
      failure, safely returns the String original state.

--- header/Utils/String.h
+++ header/Utils/String.h
@@ -7,12 +7,16 @@
 typedef struct {
     char *str;
     size_t len;
+    size_t allocSize;
 } String;
 
 String newEmptyString();
 String newString(char *str, size_t len);
 
-int stringCpy(String to, String from);
+void freeString(String *str);
+int resizeStringAlloc(String *str, size_t size);
+
+int stringCpy(String *to, String from);
 int stringCmp(String s1, String s2);
 
 #endif // INCLUDE_UTILS_STRING_H_
--- src/Utils/String.c
+++ src/Utils/String.c
@@ -1,25 +1,21 @@
-#include "Utils/String.h"
+#include <stddef.h>
 #include <stdlib.h>
 #include <string.h>
 
-String newEmptyString() { return (String){NULL, 0}; }
+#include "Utils/String.h"
+
+String newEmptyString() { return (String){NULL, 0, 0}; }
 String newString(char *str, size_t len) {
     String out = newEmptyString();
-    stringCpy(out, (String){str, len});
+    stringCpy(&out, (String){str, len, len});
     return out;
 }
 
-int stringCpy(String to, String from) {
-    if (from.len != to.len) {
-        char *aux = to.str;
-        to.str = realloc(to.str, sizeof(char) * from.len);
-        if (to.str == NULL) {
-            to.str = aux;
-            return -1;
-        }
-        to.len = from.len;
-    }
-    memcpy(to.str, from.str, from.len);
+int stringCpy(String *to, String from) {
+    if (to->allocSize < from.len && resizeStringAlloc(to, from.len) == -1)
+        return -1;
+    memcpy(to->str, from.str, from.len);
+    to->len = from.len;
     return from.len;
 }
 
@@ -30,4 +26,23 @@ int stringCmp(String s1, String s2) {
         out = s1.len > s2.len ? 1 : -1;
     }
     return out;
+}
+
+void freeString(String *str) {
+    free(str->str);
+    str->str = NULL;
+    str->len = 0;
+    str->allocSize = 0;
+}
+
+int resizeStringAlloc(String *str, size_t size) {
+    char *aux = str->str;
+    str->str = realloc(str->str, sizeof(char) * size);
+    if (str->str == NULL) {
+        str->str = aux;
+        return -1;
+    }
+    str->len = size < str->len ? size : str->len;
+    str->allocSize = size;
+    return size;
 }
