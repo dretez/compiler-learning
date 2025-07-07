82d483df3902cc753890d27767afaa91657b3603
André Maltez Nunes <andremalteznunes@sapo.pt>
2025-07-07 21:30:02 +0100

    feat: Update Tree functions

--- src/Utils/Tree.c
+++ src/Utils/Tree.c
@@ -1,46 +1,106 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
 
-#include "Utils/String.h"
 #include "Utils/Tree.h"
 #include "Utils/TreeList.h"
 
-Tree newTree() {
+Tree Tree_init() {
     return (Tree){
-        newEmptyString(),
-        NULL,
-        newTreeList(),
+        .data = NULL,
+        .parent = NULL,
+        .children = TreeList_init(),
     };
 }
 
-int treeAddNewChild(Tree *root, Tree child) {
-    child.parent = root;
-    return treeListAddTree(&root->children, child);
-}
+/***************************** MEMORY MANAGEMENT *****************************/
 
-Tree *mallocTree(Tree orig) {
+Tree *Tree_new() {
     Tree *tree = malloc(sizeof(Tree));
     if (tree == NULL)
         return NULL;
-    *tree = orig;
+    *tree = Tree_init();
     return tree;
 }
 
-void freeTree(Tree *root) {
+void Tree_clear(Tree *tree) {
+    if (tree == NULL)
+        return;
+    TreeList_clear(&tree->children);
+    Tree_removeChild(tree->parent, tree);
+    *tree = Tree_init();
+}
+
+void Tree_free(Tree *root) {
     for (uint i = 0; i < root->children.count; i++) {
         root->children.list[i]->parent = NULL;
-        freeTree(root->children.list[i]);
+        Tree_free(root->children.list[i]);
     }
     if (root->parent != NULL) {
-        treeListMoveTreeToEnd(&root->parent->children, root);
+        TreeList_mvTreeToEnd(&root->parent->children, root);
     }
-    freeTreeNode(root);
+    Tree_freeNode(root);
 }
-TreeList freeTreeNode(Tree *node) {
+
+TreeList Tree_freeNode(Tree *node) {
+    Tree *gramps = node->parent;
     for (uint i = 0; i < node->children.count; i++) {
-        node->children.list[i]->parent = NULL;
+        node->children.list[i]->parent = gramps;
     }
     TreeList childrenCopy = node->children;
-    freeString(&node->label);
     free(node);
     return childrenCopy;
+}
+
+/********************************** GETTERS **********************************/
+
+uint Tree_childCount(Tree tree) { return tree.children.count; }
+
+Tree *Tree_getChild(Tree tree, uint idx) {
+    if (tree.children.count < idx)
+        return NULL;
+    return tree.children.list[idx];
+}
+
+TreeList *Tree_getChildren(Tree *tree) { return &tree->children; }
+
+Tree *Tree_getParent(Tree tree) { return tree.parent; }
+
+void *Tree_getData(Tree tree) { return tree.data; }
+
+/******************************************************************************/
+
+void Tree_addChild(Tree *root, Tree *child) {
+    child->parent = root;
+    TreeList_add(&root->children, child);
+}
+
+void Tree_removeChild(Tree *parent, Tree *child) {
+    if (parent == NULL || child == NULL)
+        return;
+    TreeList_remove(&parent->children, child);
+}
+
+/********************************** PRINTING **********************************/
+
+void Tree_fprint(FILE *stream,
+                 void (*valuePrinter)(void *),
+                 Tree tree,
+                 uint depth) {
+    for (uint i = 0; i < depth; i++)
+        fprintf(stream, "  ");
+    valuePrinter(tree.data);
+    fprintf(stream, "Parent: %p\n", Tree_getParent(tree));
+    TreeList_fprint(stream, valuePrinter, tree.children, depth + 1);
+}
+void Tree_fprintNode(FILE *stream, void (*valuePrinter)(void *), Tree tree) {
+    valuePrinter(tree.data);
+    fprintf(stream, "Parent: %p;", Tree_getParent(tree));
+    fprintf(stream, "Child count: %d\n", Tree_childCount(tree));
+}
+void Tree_print(Tree tree, void (*valuePrinter)(void *)) {
+    Tree_fprint(stdout, valuePrinter, tree, 0);
+}
+void Tree_printNode(Tree tree, void (*valuePrinter)(void *)) {
+    Tree_fprintNode(stdout, valuePrinter, tree);
 }
--- src/Utils/TreeList.c
+++ src/Utils/TreeList.c
@@ -1,19 +1,54 @@
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "Utils/Tree.h"
 #include "Utils/TreeList.h"
 
-TreeList newTreeList() {
+TreeList TreeList_init() {
     return (TreeList){
-        NULL,
-        0,
-        0,
+        .list = NULL,
+        .count = 0,
+        .alloced = 0,
     };
 }
 
-int treeListAddTree(TreeList *tl, Tree value) {
-    Tree *new = mallocTree(value);
-    if (new == NULL)
+TreeList *TreeList_new() {
+    TreeList *list = malloc(sizeof(TreeList));
+    if (list == NULL)
+        return NULL;
+    *list = TreeList_init();
+    return list;
+}
+
+void TreeList_clear(TreeList *list) {
+    if (list == NULL || list->alloced == 0)
+        return;
+    for (uint i = 0; i < list->count; i++)
+        list->list[i]->parent = NULL;
+    free(list->list);
+    *list = TreeList_init();
+}
+
+void TreeList_free(TreeList **list) {
+    if (*list == NULL)
+        return;
+    TreeList_clear(*list);
+    free(*list);
+    *list = NULL;
+}
+
+void TreeList_freeItems(TreeList *list) {
+    if (list == NULL)
+        return;
+    for (uint i = 0; i < list->count; i++)
+        Tree_free(list->list[i]);
+    TreeList_clear(list);
+}
+
+/******************************************************************************/
+
+int TreeList_add(TreeList *tl, Tree *value) {
+    if (value == NULL)
         return -1;
 
     if (tl->count >= tl->alloced) {
@@ -21,21 +56,36 @@ int treeListAddTree(TreeList *tl, Tree value) {
         tl->list = realloc(tl->list, sizeof(Tree *) * (1 + tl->count));
         if (tl->list == NULL) {
             tl->list = aux;
-            free(new);
             return -1;
         }
         tl->alloced = tl->count + 1;
     }
-    tl->list[tl->count] = new;
+    tl->list[tl->count] = value;
     tl->count++;
     return tl->count;
 }
 
-void treeListRemoveTree(TreeList *tl, Tree *tree) {
-    treeListMoveTreeToEnd(tl, tree);
-    treeListRemoveLast(tl);
+void TreeList_remove(TreeList *list, Tree *tree) {
+    if (list == NULL)
+        return;
+    uint i = 0;
+    while (list->list[i] != tree && i < list->count)
+        i++;
+    if (i == list->count) // tree doesn't exist in list.
+        return;
+    for (; i < list->count - 1; i++)
+        list->list[i] = list->list[i + 1];
+    list->count--;
+    Tree **aux = list->list;
+    list->list = realloc(list->list, sizeof(Tree *) * (list->count));
+    if (list == NULL) {
+        list->list = aux;
+        return;
+    }
+    list->alloced = list->count;
 }
-void treeListMoveTreeToEnd(TreeList *tl, Tree *tree) {
+
+void TreeList_mvTreeToEnd(TreeList *tl, Tree *tree) {
     uint i = 0;
     while (tl->list[i] != tree && i < tl->count)
         i++;
@@ -46,7 +96,8 @@ void treeListMoveTreeToEnd(TreeList *tl, Tree *tree) {
         tl->list[i] = tl->list[i + 1];
     tl->list[i] = aux;
 }
-void treeListRemoveLast(TreeList *tl) {
+
+void TreeList_rmLast(TreeList *tl) {
     if (tl->count == 0)
         return;
     tl->count--;
@@ -57,4 +108,20 @@ void treeListRemoveLast(TreeList *tl) {
         return;
     }
     tl->alloced = tl->count;
+}
+
+void TreeList_runMethod(TreeList *list, void (*method)(Tree *)) {
+    for (uint i = 0; i < list->count; i++)
+        method(list->list[i]);
+}
+
+/******************************************************************************/
+
+void TreeList_fprint(FILE *stream,
+                     void (*valuePrinter)(void *),
+                     TreeList list,
+                     uint depth) {
+    for (uint i = 0; i < list.count; i++) {
+        Tree_fprint(stream, valuePrinter, *list.list[i], depth);
+    }
 }
