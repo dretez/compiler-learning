7c33fbd055f3f83d0ba6db130229aae7632d9e39
dretez <105381869+dretez@users.noreply.github.com>
2025-07-16 19:23:44 +0100

    refactor(AST): Update AST functions according to changes to Tree
    - Remove unique definition of AST. Now AST is simply a typedef of Tree
    - As AST is no longer its own unique type, removed all functions related
      to allocating and initializing AST objects. Tree functions should be
      used instead.

--- header/Parser/AST.h
+++ header/Parser/AST.h
@@ -4,22 +4,15 @@
 #include "Lexer/Token.h"
 #include "Utils/Tree.h"
 
-// Abstract Syntax Tree
-typedef struct {
-    Token *token;
-    Tree tree;
-} AST;
+typedef Tree AST;
 
-AST AST_init();
-AST *AST_new();
-void AST_free(AST *root);
-void AST_clear(AST *root);
+Token *AST_data(AST *tree);
 
 AST *AST_addToken(AST *root, Token *token);
 
-void AST_print(AST tree);
-void AST_fprint(FILE *stream, AST tree, uint depth);
-void AST_println(AST tree);
-void AST_fprintln(FILE *stream, AST tree, uint depth);
+void AST_print(AST *tree);
+void AST_fprint(FILE *stream, AST *tree, uint depth);
+void AST_println(AST *tree);
+void AST_fprintln(FILE *stream, AST *tree, uint depth);
 
 #endif // ! INCLUDE_PARSER_AST_H
--- src/Parser/AST.c
+++ src/Parser/AST.c
@@ -1,104 +1,76 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-#include "Parser/AST.h"
 #include "Lexer/Token.h"
+#include "Parser/AST.h"
 #include "Utils/Tree.h"
 
-void AST_setParent(AST *node, AST *parent);
-void AST_updateData(AST *ast);
 uint AST_getMaxChildren(AST *ast);
 
-AST AST_init() {
-    AST new = {
-        .token = NULL,
-        .tree = Tree_init(),
-    };
-    return new;
-}
-
-AST *AST_new() {
-    AST *ast = malloc(sizeof(AST));
-    if (ast == NULL)
-        return NULL;
-    *ast = AST_init();
-    AST_updateData(ast);
-    return ast;
-}
-
-void AST_free(AST *root) {
-    AST_clear(root);
-    free(root);
-}
-
-void AST_clear(AST *root) {
-    Tree_clear(&root->tree);
-    root->token = NULL;
+Token *AST_data(AST *tree) {
+    return Tree_getData(tree);
 }
 
 AST *AST_addToken(AST *root, Token *token) {
-    AST *tree = AST_new();
+    AST *tree = Tree_new();
     if (tree == NULL)
         return root;
-    tree->token = token;
+    Tree_setData(tree, token);
     // if (token->precedence == NONE) {
-    //     AST_setParent(tree, root);
+    //     Tree_setParent(tree, root);
     //     return root;
     // }
 
-    if (token->precedence != NONE && token->precedence >= root->token->precedence) {
-        AST_setParent(root, tree);
+    if (token->precedence != NONE &&
+        token->precedence >= AST_data(root)->precedence) {
+        Tree_setParent(root, tree);
         return tree;
     }
-    if (AST_getMaxChildren(root) < root->tree.children.count) {
-        AST_setParent(tree, root);
+    if (AST_getMaxChildren(root) < Tree_childCount(root)) {
+        Tree_setParent(tree, root);
         return root;
     }
-    AST *rootsLastChild = (AST *)(Tree_getChild(root->tree, root->tree.children.count - 1)->data);
-    if (token->precedence <= rootsLastChild->token->precedence) {
-        AST_setParent(tree, root);
+    AST *last = Tree_getChild(root, Tree_childCount(root) - 1);
+    if (token->precedence <= AST_data(last)->precedence) {
+        Tree_setParent(tree, root);
         return root;
     }
-    
-    AST_setParent(rootsLastChild, tree);
-    AST_setParent(tree, root);
+
+    Tree_setParent(last, tree);
+    Tree_setParent(tree, root);
     return root;
 }
 
-void AST_print(AST tree) {
+void AST_print(AST *tree) {
     AST_fprint(stdout, tree, 0);
 }
 
-void AST_fprint(FILE *stream, AST tree, uint depth) {
-    for(uint i = 0; i < depth; i++)
+void AST_fprint(FILE *stream, AST *tree, uint depth) {
+    for (uint i = 0; i < depth; i++)
         fprintf(stream, "  ");
-    Token_fprintln(stream, *tree.token);
+    Token_fprintln(stream, *AST_data(tree));
     fprintf(stream, "\n");
 
-    for (uint i = 0; i < Tree_childCount(tree.tree); i++) {
-        AST_fprint(stream, *((AST *)(Tree_getChild(tree.tree, i)->data)), depth + 1);
+    for (uint i = 0; i < Tree_childCount(tree); i++) {
+        Tree *ctree = Tree_getChild(tree, i);
+        AST_fprint(stream, ctree, depth + 1);
     }
 }
 
-void AST_println(AST tree) {
+void AST_println(AST *tree) {
     AST_fprintln(stdout, tree, 0);
 }
-void AST_fprintln(FILE *stream, AST tree, uint depth) {
+void AST_fprintln(FILE *stream, AST *tree, uint depth) {
     AST_fprint(stream, tree, depth);
     fprintf(stream, "\n");
 }
 
-void AST_setParent(AST *node, AST *parent) {
-    Tree_removeChild(node->tree.parent, &node->tree);
-    Tree_addChild(&parent->tree, &node->tree);
-}
-
-void AST_updateData(AST *ast) {
-    ast->tree.data = ast;
+void Tree_setParent(AST *node, AST *parent) {
+    Tree_removeChild(Tree_getParent(node), node);
+    Tree_addChild(parent, node);
 }
 
 uint AST_getMaxChildren(AST *ast) {
-    switch (ast->token->type) {
+    switch (AST_data(ast)->type) {
     case PLUS:
     case MINUS:
     case TIMES:
